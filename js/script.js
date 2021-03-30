2db405b9f4ded9f4c2e65829c7c103b8023b171f
Rocco Colonna <colonna.rocco2000@gmail.com>
2021-03-30 19:50:41 +0200

    da finire js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var calcolaButton = document.getElementById('bottone-calcolatore');
+calcolaButton.addEventListener ('click', function() {
+    
+    var nomececk = document.getElementById('cheese');
+    console.log(nomececk.value)
+
+});
